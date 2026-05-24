9dd47d46b757cec7a32ebbc33e48e1685ba716da
jorben12 <88739935+jorben12@users.noreply.github.com>
2026-05-24 22:16:30 +0200

    meer

--- PartOne2_0.m
+++ PartOne2_0.m
@@ -3,10 +3,10 @@ clear all
 close all
 
 z1 =[0+50i 0-50i];
-p1 =[-1 -2000];
+p1 =[-1 -2500];
 
 z2 =  [0+500i 0-500i];
-p2 =  [-1 -300000];
+p2 =  [-1 -250000];
 
 k = 1;
 sys1 = zpk(z1,p1,k);
@@ -24,8 +24,8 @@ bode(sys2)
 [A,B] = tfdata(sys1)
 [A,B] = tfdata(sys2)
 
-A1 =[1/44.99^2 0 1];
-B1 = [1/44.99^2 0.988 1];
+A1 =[1/50^2 0 1];
+B1 = [1/50^2 0.8 1];
 
 A2 = [1/500^2 0 1];
 B2 = [1/500^2 1.2 1];
@@ -36,7 +36,6 @@ sysC = sysA*sysB;
 
 figure(2)
 bode(sysC)
-
 
 figure(3)
 pzmap(sys)
@@ -59,11 +58,27 @@ step(sys);
 
 
 [Gm,Pm,Wcp, Wvp] = margin(sys);
-Gm_dB = 20*log10(Gm)
+Gm_dB = 20*log10(Gm);
 Wcp
 
 figure(4);
 rlocus(sys);
+
+%p contorler voorbereiden
+t = (0:0.0001:0.75)';
+k = 63;
+KI = 0;
+KD = 0;
+TaS = 0;
+tdelay = 0;
+
+out = sim('PID_controler.slx');
+
+figure(5)
+plot(t,out.simout);
+hold on
+plot (t,out.step);
+
 
 %%
 %Part 3 
@@ -79,23 +94,25 @@ Gm
 Wcp
 T =2 * pi/Wcp
 
-Kzn = 0.6*Gm;
-TIzn = T/2;
-TDzn = T/8;
+Kzn = 0.6*Gm
+TIzn = T/2
+TDzn = T/8
 
 %voorbereidingen simulink inputs met KZ parmater
 t = (0:0.0001:0.75)';
 
 k = Kzn
-KI = 1/(TIzn*Kzn)
-KD = TDzn/Kzn
-TaS = 0.01
+KI = Kzn/TIzn
+KD = TDzn*Kzn
+TaS = 0.0001
 tdelay = 0;
 
 out = sim('PID_controler.slx');
 
 figure(2)
 plot(t,out.simout);
+hold on
+plot (t,out.step);
 
 %PID contorler met verbeterde parameters
 k = 18;
@@ -123,15 +140,15 @@ KI = 85;
 KD = 0.63;
 TaS = 1/10000;
 
-
 s = tf('s');
-TC = k + KI/s + KD*s/(TaS + s);
+TC = k+KI/s+s*KD/(TaS*s+1);
 
-Hol = sys * TC;
-margin(Hol);
-[Gm,Pm,Wcp, Wvp] = margin(Hol);
+Tol = sys * TC;
+margin(Tol);
+[Gm,Pm,Wcp, Wvp] = margin(Tol);
 
 tmax = (Pm*pi)/(180*Wvp)
+
 
 t = (0:0.001:1)';
 tdelay = 0.0006061; %marginely stabel
